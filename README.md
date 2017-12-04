887f41665f7b3d74ef4b2145e7651cdce52d7c64
Mat Moore <matmoore@users.noreply.github.com>
2017-12-04 13:59:44 +0000

    Get something working

---
+++ README.md
@@ -0,0 +1,29 @@
+# Synonym tester
+
+CLI utility for quickly rebuilding a stripped down GOV.UK search index,
+and swapping out synonym rules.
+
+This is currently a proof of concept.
+
+It requires access to an elasticsearch cluster containing GOV.UK search indexes.
+
+# Usage
+
+`./bin/synonym_tester`
+
+## Completed
+- Create a test index with customised analysis settings
+
+## Unfinished
+- Define a useful mapping
+- Specify synonym rules to test in isolation
+- Run test searches
+- Report search results
+
+### Dependencies
+
+- [https://github.com/elastic/elasticsearch-ruby](Elasticsearch ruby)
+
+## Licence
+
+[MIT License](LICENCE)
--- lib/synonym_tester.rb
+++ lib/synonym_tester.rb
@@ -1,10 +1,17 @@
+require 'pp'
+require 'pry-byebug'
+
 class SynonymTester
-  def initialize(es_client, source_index_pattern: "govuk,mainstream,government,detailed", source_index_fields: %w(title description indexable_content), index_list: "index_synonym", search_list: "search_synonym")
+  INDEX_FILTER = "index_synonym"
+  SEARCH_FILTER = "search_synonym"
+  INDEX_ANALYZER = "with_index_synonyms"
+  SEARCH_ANALYZER = "with_search_synonyms"
+  TEST_INDEX_NAME = "test_index"
+
+  def initialize(es_client, source_index_pattern: "govuk,mainstream,government,detailed", source_index_fields: %w(title description indexable_content))
     @client = es_client
     @source_index_fields = source_index_fields
     @source_index_pattern = source_index_pattern
-    @index_list = index_list
-    @search_list = search_list
   end
 
   def run(queries:, doc_ids:, search_synonyms:, index_synonyms:)
@@ -28,32 +35,80 @@ private
   end
 
   def create_index(index_synonyms, search_synonyms)
-    source = source_index_settings
-    analysis = source["analysis"]
+    begin
+      client.indices.delete(index: TEST_INDEX_NAME)
+    rescue Elasticsearch::Transport::Transport::Errors::NotFound
+    end
+
+    analysis = source_index_analysis_settings
     filter = analysis["filter"]
 
     payload = {
-      "analysis" => analysis.merge(
-        "filter" => filter.merge(
-          index_list => {"type" => "synonym", "synonyms" => index_synonyms},
-          search_list => {"type" => "synonym", "synonyms" => search_synonyms}
+      "settings" => {
+        "analysis" => analysis.merge(
+          "filter" => filter.merge(
+            INDEX_FILTER => {"type" => "synonym", "synonyms" => index_synonyms},
+            SEARCH_FILTER => {"type" => "synonym", "synonyms" => search_synonyms}
+          )
         )
-      )
+      },
+      "mappings" => test_mappings,
+      "index" => {"number_of_shards" => 1, "number_of_replicas" => 0}
     }
 
+    client.indices.create(
+      index: TEST_INDEX_NAME,
+      body: payload
+    )
   end
 
   def copy_data(doc_ids)
+    indexes = client.indices.get(index: source_index_pattern)
+    puts "reindexing docs from #{indexes.keys}"
 
+    indexes.keys.each do |source_index|
+
+      payload = {
+        "source": {
+          "index" => source_index,
+          "query" => {
+            "bool" => {
+              "filter" => {
+                "ids" => {"values" => doc_ids}
+              }
+            }
+          }
+        },
+        "dest": {
+          "index" => TEST_INDEX_NAME
+        }
+      }
+
+      results = client.reindex(body: payload, wait_for_completion:true)
+    end
   end
 
-  def source_index_settings
+  def test_mappings
     {
-      "analysis" => {
-        "filter" => {}
+      "test_type" => {
+        "properties" => source_index_fields.map do |field_name|
+          [
+            field_name,
+            {
+              "type": "string",
+              "analyzer": INDEX_ANALYZER,
+              "search_analyzer": SEARCH_ANALYZER
+            }
+          ]
+        end.to_h
       }
     }
   end
 
-  attr_reader :client, :source_index_pattern, :source_index_fields, :index_list, :search_list
+  def source_index_analysis_settings
+    results = client.indices.get(index: source_index_pattern)
+    results.values.first["settings"]["index"]["analysis"]
+  end
+
+  attr_reader :client, :source_index_pattern, :source_index_fields
 end
